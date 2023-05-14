0ca6d7bab7ca183fb8f30b3e442a12b06ca71241
Michał Rojowiec <michal.rojowiec@gmail.com>
2023-05-14 19:30:38 +0200

    Zadanie domowe dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.Design;
+
+string name = "Ewa";
+int age = 33;
+String sex = "Female";
+
+if (sex == "Female")
+{
+    if (name == "Ewa" && age >= 33)
+    {
+        Console.WriteLine("Ewa lat 33");
+    }
+    else if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+}
+else if (age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else if (age >= 18)
+{
+    Console.WriteLine("Pełnoletni mężczyzna");
+}
+    
