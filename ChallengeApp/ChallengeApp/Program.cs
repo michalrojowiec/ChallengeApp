c64ca5bf504fb95f926dacd3d8698ec4eab66608
Michał Rojowiec <michal.rojowiec@gmail.com>
2023-05-15 11:01:36 +0200

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -7,10 +7,14 @@ if (sex == "Female")
     if (name == "Ewa" && age == 33)
     {
         Console.WriteLine("Ewa lat 33");
-    }
-    else if (age < 30)
+    } 
+    else if (age <30)
     {
         Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if (age < 33)
+    {
+        Console.WriteLine("Kobieta poniżej 33 lat");
     }
     else if (age > 33)
     {
