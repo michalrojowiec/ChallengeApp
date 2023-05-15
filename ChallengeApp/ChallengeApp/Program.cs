0c7ca6cb40c6fecb0022119d84b1e857343edbfa
Michał Rojowiec <michal.rojowiec@gmail.com>
2023-05-15 10:41:52 +0200

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,18 +1,20 @@
-﻿using System.ComponentModel.Design;
-
-string name = "Ewa";
+﻿string name = "Ewa";
 int age = 33;
-String sex = "Female";
+string sex = "Female";
 
 if (sex == "Female")
 {
-    if (name == "Ewa" && age >= 33)
+    if (name == "Ewa" && age == 33)
     {
         Console.WriteLine("Ewa lat 33");
     }
     else if (age < 30)
     {
         Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if (age > 33)
+    {
+        Console.WriteLine("Kobieta powyżej 33 lat");
     }
 }
 else if (age < 18)
