83c0f8341a38d08947238148d51175a49d47a94a
michalrojowiec <99677218+michalrojowiec@users.noreply.github.com>
2023-05-21 19:04:29 +0200

    Zadanie dzień 8

---
+++ ChallengeApp/ChallengeApp.Test/TypeTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Reflection.Metadata;
+
+namespace ChallengeApp.Test
+{
+    public class TypeTests
+    {
+        [Test]
+        public void TestingIntigers()
+        { 
+            int number1 = 1;
+            int number2 = 2;
+
+            Assert.AreNotEqual(number1, number2);
+        }
+
+        [Test]
+        public void TestingDifferentEmployees()
+        {
+            var user1 = GetUser("Michał");
+            var user2 = GetUser("Wojtek");
+
+
+            Assert.AreNotEqual(user1, user2);
+        }
+
+        [Test]
+        public void TestingFloats()
+        {
+            float number1 = 15022;
+            float number2 = 15022;
+
+            Assert.AreEqual(number1, number2);
+        }
+
+        [Test]
+        public void TestingStrings()
+        {
+            string name1 = "Michał";
+            string name2 = "Michał";
+
+            Assert.AreEqual(name1, name2);
+        }
+
+
+
+        private Employee GetUser(string name)
+        {
+            return new Employee("name", "surname", "age");
+        }
+    }
+}
+
+
