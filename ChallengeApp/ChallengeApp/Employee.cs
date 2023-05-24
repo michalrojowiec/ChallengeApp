31b90ae3d848f5b64be85343e3d6921206873e73
michalrojowiec <99677218+michalrojowiec@users.noreply.github.com>
2023-05-24 17:36:46 +0200

    Zadanie dzień 10

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -15,8 +15,51 @@
 
         public void AddGrade(float grade)
         {
-            this.grades.Add(grade);
+            int valueInInt = (int)grade;
+            float f = valueInInt;
+
+            if (grade >= 0 && grade <= 100)
+            {
+                this.grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("Invaild grade value");
+            }
+
         }
+
+        public void AddGrade(string grade)
+        {
+            if (float.TryParse(grade, out float result))
+            {
+                this.AddGrade(result);
+            }
+            else
+            {
+                Console.WriteLine("String is not Float");
+            }
+
+        }
+
+        public void AddGrade(int grade)
+        {
+            float gradeAsFloat = (float)grade;
+            this.AddGrade(gradeAsFloat);
+        }
+
+        public void AddGrade(double grade)
+        {
+            float gradeAsFloat = (float)grade;
+            this.AddGrade(gradeAsFloat);
+        }
+
+        public void AddGrade(long grade)
+        {
+            float gradeAsFloat = (float)grade;
+            this.AddGrade(gradeAsFloat);
+        }
+
 
         public Statistics GetStatistics()
         {
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,9 +1,11 @@
 ﻿using ChallengeApp;
 
 var employee = new Employee("Michał", "Kowalik");
+employee.AddGrade("Michał");
+employee.AddGrade("500");
+employee.AddGrade(8f);
 employee.AddGrade(5);
-employee.AddGrade(8);
-employee.AddGrade(4);
+employee.AddGrade(7.5);
 
 var statistics = employee.GetStatistics();
 
